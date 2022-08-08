a65f28e11f47b1e124aa1806b5c0721ec5adc0fa
tannuahuja <59529688+tannuahuja@users.noreply.github.com>
2022-08-09 02:22:59 +0530

    Add files via upload

---
+++ netflix landing page/css/js/main.js
@@ -0,0 +1,22 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+// select tab content item
+function selectItem(e){
+    removeBorder();
+    removeShow();
+// add border to current tab
+   this.classList.add('tab-border');
+// //  Grab content items from DOM
+//   const tabContentItem = querySelector(`#${this.id}-content`);  
+// Add show class
+  tabContentItem.classList.add('show');  
+}
+function removeBorder(){
+    tabItems.forEach(item=>item.classList.remove('tab-border'));
+}
+function removeShow(){
+    tabContentItems.forEach(itme=>item.classList.remove('show'));
+}
+// listen for tab click
+ tabItems.forEach(item=>item.addEventListener('click',selectItem));
